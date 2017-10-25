d886e17b6f79c19909a4f2461348265c7ed2ca2e
Vitaliy Banit <stamigos@Vitaliys-MacBook-Pro.local>
2017-10-25 22:07:46 +0300

    rest framework added

--- eaas_cloud/settings.py
+++ eaas_cloud/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'DEFAULT_PAGINATION_CLASS': [
+        'rest_framework.pagination.PageNumberPagination'
+    ],
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
